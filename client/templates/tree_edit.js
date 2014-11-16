2384ac8d3b242dc3eaf312038668b7a52fed95f1
LAURENT POULAIN <laurent.poulain@gmail.com>
2014-11-15 20:58:36 -0500

    Completed the move of a branch

--- client/templates/tree_edit.js
+++ client/templates/tree_edit.js
@@ -9,17 +9,35 @@ Template.treeEdit.events({
         e.preventDefault();
 
         var currentTreeId = this._id;
+        var branch = this;
 
         var treeProperties = {
             name: $(e.target).find('[name=name]').val(),
             path: $(e.target).find('[name=path]').val(),
+            level: $(e.target).find('[name=path]').val().split(/\|/).length-1
         }
+
+        var updateSubtree = (this.path != treeProperties.path ||
+                             this.name != treeProperties.name);
+
+        var newParentPath = treeProperties.path + '|' + treeProperties.name;
+        var oldParentPathLength = this.path.length + 1 + this.name.length;
 
         Tree.update(currentTreeId, {$set: treeProperties}, function(error) {
             if (error) {
                 // display the error to the user
                 alert(error.reason);
             } else {
+                if (updateSubtree) {
+                    var subBranches = Tree.find({path: { $regex: '^' + branch.path.replace('|', '\\|') + '\\|' + branch.name }}).fetch();
+                    _.each(subBranches, function(subBranch) {
+//                        subBranch.path = newPath + subBranch.path.substr(oldPathLength);
+                        var newPath = newParentPath + subBranch.path.substr(oldParentPathLength);
+                        Tree.update({ _id: subBranch._id },
+                                    { $set: {path: newPath, level: newPath.split(/\|/).length-1 } });
+                    });
+                }
+
                 Router.go('treeManage');
             }
         });
@@ -29,16 +47,20 @@ Template.treeEdit.events({
         e.preventDefault();
         if (confirm("Delete this branch?")) {
             var currentTreeId = this._id;
-	    var subtrees = Tree.findOne({path: this.path + '|' + this.name});
-	    if (typeof subtrees != 'undefined') {
-		alert('Please delete any subtree before');
-		return;
-	    }
-	    var notes = Notes.findOne({section: this._id});
-	    if (typeof notes != 'undefined') {
-		alert('Please delete any associated note before');
-		return;
-	    }
+
+            // Checks that there is no subtree
+            var subtrees = Tree.findOne({path: this.path + '|' + this.name});
+            if (typeof subtrees != 'undefined') {
+                alert('Please delete any subtree before');
+                return;
+            }
+
+            // Checks there are no note in the branch
+            var notes = Notes.findOne({section: this._id});
+            if (typeof notes != 'undefined') {
+                alert('Please delete any associated note before');
+                return;
+            }
             Tree.remove(currentTreeId);
             Router.go('treeManage');
         }
